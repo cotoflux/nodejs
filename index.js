227cb097db82940b4748f04c363a5cd0cad08db9
cotoflux <35181092+cotoflux@users.noreply.github.com>
2019-03-05 13:11:26 +0100

    New version aleatory name and number + html
    This is my after work class, I played with the new library Chance has. The teacher showed us how to show an aleatory name I played with aleatory numbers and left integer showing aleatory numbers. Plus I wanted to display a previous work with html showing as well in my initial commit.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // servidor http
 
-// cargar libreria http
+// charging the http library
 
 const http = require('http');
 
-// definir un servidor 
+// I do start chance and load chance then is mandatory to instantiate Chance so I can use it, check library documentation http://chancejs.com/
+const Chance = require('chance');
+const chance = new Chance();
+
+// definition of my server
 const server = http.createServer(function(request, response) {
 
         response.writeHead(200,{'Content-Type': 'text/html'});
-        response.end('<h1>This is a test </h1><br> Wake up, <b>Neo eres genial</b> <h3>Was checking how could I put much more information in this server</h3> <p>To me the problem here is indentation</pr>');
-
+        response.end('Wake up, now you will see a different name every time ' + chance.name() + 'and a random number' +chance.integer() + ' Wow!!'+ '<h1>Can you see my HTML working!!</h1>' );
+        
 })
 
-// arrancar el servidor
+// starting my local server
 server.listen(1337,'127.0.0.1')
 console.log('Servidor arrancado en http://127.0.0.1:1337');
